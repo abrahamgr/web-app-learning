1c8df55ee5cedd007c6f69438cacca08986cd4f4
Abraham Galindo <abraham.garo@gmail.com>
2024-05-21 14:08:42 -0600

    fix: set prod for drizzle migration

--- web/04-nextjs/drizzle.config.ts
+++ web/04-nextjs/drizzle.config.ts
@@ -2,9 +2,8 @@
 import './envConfig'
 import { defineConfig } from 'drizzle-kit'
 import { dbConfig } from '@/const/dbConfig'
-import { isProd } from '@/const/clientConfig'
 
-const localConfig = defineConfig({
+export default defineConfig({
   schema: './src/db/schema.ts',
   out: './drizzle',
   dialect: 'postgresql',
@@ -15,13 +14,3 @@ const localConfig = defineConfig({
     database: dbConfig.database,
   },
 })
-
-const vercelConfig = defineConfig({
-  schema: './src/db/schema.ts',
-  dialect: 'postgresql',
-  dbCredentials: {
-    url: process.env.POSTGRES_URL as string,
-  },
-})
-
-export default isProd ? vercelConfig : localConfig
